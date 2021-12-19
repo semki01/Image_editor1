20a0223e83d65d976837d7f8ee287b69f60358ac
Semki 01 <91430213+semki01@users.noreply.github.com>
2021-12-19 19:26:17 +0200

    Update Image_editor.py

--- Image_editor.py
+++ Image_editor.py
@@ -1,33 +1,64 @@
-from PIL import Image
-from PIL import ImageFilter
-from PIL import ImageEnhance
+import os
+from PyQt5.QtWidgets import (QApplication, QWidget, QFileDialog, QLabel, QPushButton, QListWidget, QHBoxLayout, QVBoxLayout)
+from PyQt5 import Qt
 
-with Image.open('mops-mops-457.jpg') as pic_original:
-    print('Изображение открыто\nРазмер:',pic_original.size)
-    print('Формат:', pic_original.format)
-    print('Тип:', pic_original.mode)
-    pic_original.show()
+app = QApplication([])
+win = QWidget()
+win.setWindowTitle('Image Editor Pro')
+win.resize(700, 500)
 
-    #Делаем картинку серую
-    pic_gray = pic_original.convert('L')
-    pic_gray.save('gray.jpg')
-    print('Изображение открыто\nРазмер:',pic_gray.size)
-    print('Формат:', pic_gray.format)
-    print('Тип:', pic_gray.mode)
-    pic_gray.show()
+lb_image = QLabel('Картинка')
+btn_dir = QPushButton('Папка')
+btn_left = QPushButton('Лево')
+btn_right = QPushButton('Право')
+btn_bw = QPushButton('Ч/Б')
+btn_sharp = QPushButton('Резкость')
+btn_mirror = QPushButton('Зеркало')
+lw_files = QListWidget()
 
-    #Делаем картинку размытой
-    pic_blured = pic_original.convert('L')
-    pic_blured.save('blured.jpg')
-    print('Изображение открыто\nРазмер:',pic_blured.size)
-    print('Формат:', pic_blured.format)
-    print('Тип:', pic_blured.mode)
-    pic_blured.show()
 
-    #Зеркальное отражение
-    pic_mirror = pic_original.convert('L')
-    pic_mirror.save('mirror.jpg')
-    print('Изображение открыто\nРазмер:',pic_mirror.size)
-    print('Формат:', pic_mirror.format)
-    print('Тип:', pic_mirror.mode)
-    pic_mirror.show()
+row = QHBoxLayout()
+main_layout = QHBoxLayout()
+col_1 = QVBoxLayout()
+col_2 = QVBoxLayout()
+
+col_1.addWidget(btn_dir)
+col_1.addWidget(lw_files)
+row.addWidget(btn_left)
+row.addWidget(btn_right)
+row.addWidget(btn_bw)
+row.addWidget(btn_sharp)
+row.addWidget(btn_mirror)
+
+col_2.addWidget(lb_image)
+col_2.addLayout(row)
+main_layout.addLayout(col_1, 2)
+main_layout.addLayout(col_2, 8)
+
+work_dir = ''
+
+def choose_work_dir():
+    global work_dir
+    work_dir = QFileDialog.getExistingDirectory()
+
+def filter(files, extensions):
+    result = []
+    for file in files:
+        for ext in extensions:
+            if file.endswith(ext):
+                result.append(file)
+    return result
+
+def show_file_name_list():
+    choose_work_dir()
+    extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
+    filenames = filter(os.listdir(work_dir), extensions)
+    lw_files.clear()
+    for filename in filenames:
+        lw_files.addItem(filename)
+
+btn_dir.clicked.connect(show_file_name_list)        
+
+win.setLayout(main_layout)
+win.show()
+app.exec_()
