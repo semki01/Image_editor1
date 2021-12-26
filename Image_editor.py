a8ebc2461b430c506f56ab89846c6d0a21205a0e
Semki 01 <91430213+semki01@users.noreply.github.com>
2021-12-26 19:31:15 +0200

    Update Image_editor.py

--- Image_editor.py
+++ Image_editor.py
@@ -1,6 +1,10 @@
 import os
 from PyQt5.QtWidgets import (QApplication, QWidget, QFileDialog, QLabel, QPushButton, QListWidget, QHBoxLayout, QVBoxLayout)
-from PyQt5 import Qt
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PIL import Image
+
+
 
 app = QApplication([])
 win = QWidget()
@@ -59,6 +63,51 @@ def show_file_name_list():
 
 btn_dir.clicked.connect(show_file_name_list)        
 
+class ImageProcessor:
+    def __init__(self):
+        self.image = None
+        self.dir = None
+        self.filename = None
+        self.save_dir = 'Modified/'
+    
+    def loadImage(self, dir, filename):
+        self.dir = dir
+        self.filename = filename
+        image_path = os.path.join(dir, filename)
+        self.image = Image.open(image_path)
+
+    def saveImage(self):
+        path = os.path.join(self.dir, self.save_dir)
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        image_path = os.path.join(path, self.filename)
+        self.image.save(image_path)
+
+    def showImage(self, path):
+        lb_image.hide()
+        pixmapimage = QPixmap(path)
+        w, h = lb_image.width(), lb_image.height()
+        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        lb_image.setPixmap(pixmapimage)
+        lb_image.show()
+    
+    def do_bw(self):
+        self.image = self.image.convert('L')
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+workimage = ImageProcessor()
+
+def showChosenImage():
+    if lw_files.currentRow() >= 0:
+        filename = lw_files.currentItem().text()
+        workimage.loadImage(work_dir, filename)
+        image_path = os.path.join(workimage.dir, workimage.filename)
+        workimage.showImage(image_path)
+
+lw_files.currentRowChanged.connect(showChosenImage)
+btn_bw.clicked.connect(workimage.do_bw)
 win.setLayout(main_layout)
 win.show()
 app.exec_()
